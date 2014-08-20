bc94d11377d85b5b1194e4cc9d97fd947d1019b2
Oya <marciooya@gmail.com>
2014-08-20 14:40:53 -0300

    touch y

--- balls/circle-game.js
+++ balls/circle-game.js
@@ -210,7 +210,7 @@ var cg = {
       if(!cg.paused) {
         cg.player.x = e.clientX
         if(cg.config.touchmove){
-          cg.player.y = e.clientY - (cg.player.radius *4)
+          cg.player.y = e.clientY - 100
         }
         else{
           cg.player.y = e.clientY
